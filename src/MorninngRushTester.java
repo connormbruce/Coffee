cbc1ab11e0464bd980d06df11ae58ef630927d03
Connor Bruce <connormbruce@ou.edu>
2018-11-07 14:00:21 -0600

    Changed where the button is

--- src/MorninngRushTester.java
+++ src/MorninngRushTester.java
@@ -1,12 +1,19 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.Timer;
+import javax.swing.JButton;
+import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 
 public class MorninngRushTester 
 {	
+	JFrame frame = new JFrame();
+	JButton button = new JButton("Add Coffee");
+	
 	private static final int DELAY = 1000;
 
+	final static int initalGulps = 5;
+	
 	public MorninngRushTester() 
 	{
 	}
@@ -27,22 +34,25 @@ public class MorninngRushTester
 			{
 				if(gulps > 0)
 				{
-					System.out.println(gulps);
-					--gulps;
+					System.out.println(gulps);	
 				}
 				else if(gulps == 0)
 				{
 					System.out.println("Go to school");
+					JOptionPane.showMessageDialog(null, "Make another");
+					gulps = initalGulps;
 					System.exit(0);
 				}
+				--gulps;
 			}		
 		}
 		
-		DrinkCoffee dc = new DrinkCoffee(5);
+		DrinkCoffee dc = new DrinkCoffee(initalGulps);
 		Timer t = new Timer(DELAY, dc);
 		t.start();
 		
-		JOptionPane.showMessageDialog(null, "Make another");
+		JOptionPane.showMessageDialog(null, "Just about to go to school");
+		System.out.println("Just about to go to school");
 		System.exit(0);
 	}
 }
